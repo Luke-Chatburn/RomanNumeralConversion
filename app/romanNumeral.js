e9e64813b4012f58488d2bb6d06afa5eaa09918c
Luke Chatburn <luke.jp.chatburn@gmail.com>
2017-02-23 23:03:07 +0000

    Working version

--- app/romanNumeral.js
+++ app/romanNumeral.js
@@ -1,13 +1,21 @@
 const romanNumerals = function () {};
 
+const romanNumeral = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+const numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+
 romanNumerals.prototype.generate = function(number) {
-    if (3 === n)
-        return "III";
+    let result = "";
 
-    if (2 === n)
-        return "II";
+    if (number < 1 || number > 3999) 
+        return "Number out of range";
 
-    return "I";
+    for (let i = 0; i < numbers.length; i++)
+        while(number >= numbers[i]) {
+            result += romanNumeral[i];
+            number -= numbers[i];
+        }
+
+    return result;
 }
 
 module.exports = new romanNumerals();
--- test/test.js
+++ test/test.js
@@ -1,14 +1,22 @@
+const romanNumerals = require("../app/romanNumeral.js");
 const mocha = require("mocha");
 const chai = require("chai");
 const expect = chai.expect;
 
 describe("RomanNumerals conversion", () => {
     it("Converts numbers to Roman numerals", () => {
-        const romanNumerals = require("../app/romanNumeral.js");
-
         expect(romanNumerals.generate(1)).to.equal("I");
         expect(romanNumerals.generate(2)).to.equal("II");
         expect(romanNumerals.generate(3)).to.equal("III");
+        expect(romanNumerals.generate(4)).to.equal("IV");
+
+        expect(romanNumerals.generate(1323)).to.equal("MCCCXXIII");
+
+    });
+
+    it("Will handle less than 1 or greater than 3999", () => {
+        expect(romanNumerals.generate(0)).to.equal("Number out of range");
+        expect(romanNumerals.generate(4689)).to.equal("Number out of range");
 
     });
 });
