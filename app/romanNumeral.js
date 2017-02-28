097f3f72101c0e3176e83a30fca439a9adffee8e
Luke Chatburn <luke.jp.chatburn@gmail.com>
2017-02-28 22:18:02 +0000

    Added in roman numeral conversion, plus refactor

--- app/romanNumeral.js
+++ app/romanNumeral.js
@@ -1,21 +1,57 @@
-const romanNumerals = function () {};
-
-const romanNumeral = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
-const numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-
-romanNumerals.prototype.generate = function(number) {
-    let result = "";
-
-    if (number < 1 || number > 3999) 
-        return "Number out of range";
-
-    for (let i = 0; i < numbers.length; i++)
-        while(number >= numbers[i]) {
-            result += romanNumeral[i];
-            number -= numbers[i];
+class romanNumerals {
+    constructor() {
+        const numeral = (value, numeral) => {
+            return {value: value, numeral: numeral};
         }
 
-    return result;
-}
+        this.numeralList = [
+            numeral(1000, "M"),
+            numeral(900, "CM"),
+            numeral(500, "D"),
+            numeral(400, "CD"),
+            numeral(100, "C"),
+            numeral(90, "XC"),
+            numeral(50, "L"),
+            numeral(40, "XL"),
+            numeral(10, "X"),
+            numeral(9, "IX"),
+            numeral(5, "V"),
+            numeral(4, "IV"),
+            numeral(1, "I")
+        ];
+    }
 
+    toRoman(number) {
+        let result = "";
+
+        // stop any out of range numbers.
+        if (number < 1 || number > 3999)
+            return false;
+
+        for (let n of this.numeralList)
+            for(; number >= n.value; number -= n.value)
+                result += n.numeral;
+
+        // if no result return false
+        return result === "" ? false : result;
+    }
+
+    fromRoman(string) {
+        let result = 0;
+        let invalidNumeral
+
+        // Prevent invalid characters
+        for (let s of string) {
+            invalidNumeral = this.numeralList.filter(n => !n.numeral.indexOf(s));
+            if (invalidNumeral.length < 1)
+                return false;
+        }
+        
+        for (let n of this.numeralList)
+            for(; string.indexOf(n.numeral) === 0; string = string.replace(n.numeral, ""))
+                result += n.value;
+
+        return result;
+    }
+}
 module.exports = new romanNumerals();
--- test/test.js
+++ test/test.js
@@ -5,18 +5,38 @@ const expect = chai.expect;
 
 describe("RomanNumerals conversion", () => {
     it("Converts numbers to Roman numerals", () => {
-        expect(romanNumerals.generate(1)).to.equal("I");
-        expect(romanNumerals.generate(2)).to.equal("II");
-        expect(romanNumerals.generate(3)).to.equal("III");
-        expect(romanNumerals.generate(4)).to.equal("IV");
-
-        expect(romanNumerals.generate(1323)).to.equal("MCCCXXIII");
-
+        expect(romanNumerals.toRoman(1)).to.equal("I");
+        expect(romanNumerals.toRoman(2)).to.equal("II");
+        expect(romanNumerals.toRoman(3)).to.equal("III");
+        expect(romanNumerals.toRoman(4)).to.equal("IV");
+        expect(romanNumerals.toRoman(1323)).to.equal("MCCCXXIII");
     });
 
-    it("Will handle less than 1 or greater than 3999", () => {
-        expect(romanNumerals.generate(0)).to.equal("Number out of range");
-        expect(romanNumerals.generate(4689)).to.equal("Number out of range");
+    it("Won't accept out of range numbers", () => {
+        expect(romanNumerals.toRoman(-1367)).to.be.false;
+        expect(romanNumerals.toRoman(0)).to.be.false;
+        expect(romanNumerals.toRoman(4000)).to.be.false;
+    });
 
+    it("Converts Roman Numerals to numbers", () => {
+        expect(romanNumerals.fromRoman("I")).to.equal(1);
+        expect(romanNumerals.fromRoman("II")).to.equal(2);
+        expect(romanNumerals.fromRoman("III")).to.equal(3);
+        expect(romanNumerals.fromRoman("IV")).to.equal(4);
+        expect(romanNumerals.fromRoman("MCCCXXIII")).to.equal(1323);
+    });
+
+    it("Won't accept incorrect Roman numerals", () => {
+        expect(romanNumerals.fromRoman("A")).to.be.false;
+        expect(romanNumerals.fromRoman("AB")).to.be.false;
+        expect(romanNumerals.fromRoman("ABI")).to.be.false;
+        expect(romanNumerals.fromRoman("MCCCXXIVAMI")).to.be.false;
+    });
+
+    it("Won't accept invalid characters", () => {
+        expect(romanNumerals.fromRoman("&")).to.be.false;
+        expect(romanNumerals.toRoman("&")).to.be.false;
+        expect(romanNumerals.fromRoman("^&*(£$%)")).to.be.false;
+        expect(romanNumerals.toRoman("^&*(£$%)")).to.be.false;
     });
 });
